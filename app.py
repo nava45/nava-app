a08065dc89e3a7ad05bd26dca854c8675f068674
nava <navaneethanit@gmail.com>
2014-08-06 13:24:43 +0530

    redesign1

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello world!"
+    return "welcome to poem background designer app!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
