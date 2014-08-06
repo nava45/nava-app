2f8d42c3b1034c13339013b7e5c2c020d0165868
nava <navaneethanit@gmail.com>
2014-08-06 18:45:15 +0530

    add app basic layout

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import os
-from flask import Flask
+from flask import Flask, request, render_template, send_from_directory, flash, redirect, url_for
+from test import draw
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY') or 't0p s3cr3t'
 
-@app.route("/")
+@app.route("/", methods=('GET', 'POST'))
 def hello():
-    return "welcome to poem background designer app!"
+    poem = request.form.get('poem',None)
+    print poem
+    draw('static/images/img.jpg','static/fonts/TAMu_Kadampari.ttf',(44, 53, 57), poem)
+    return render_template('index.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
