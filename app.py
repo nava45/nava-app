3f856cf85bf0fbd829e4f4731d1bef23821ad23c
nava <navaneethanit@gmail.com>
2014-08-06 18:47:21 +0530

    added fonts and files 1

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, request, render_template, send_from_directory, flash, redirect, url_for
-from test import draw
+#from test import draw
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY') or 't0p s3cr3t'
@@ -9,7 +9,7 @@ app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY') or 't0p s3cr3t'
 def hello():
     poem = request.form.get('poem',None)
     print poem
-    draw('static/images/img.jpg','static/fonts/TAMu_Kadampari.ttf',(44, 53, 57), poem)
+    #draw('static/images/img.jpg','static/fonts/TAMu_Kadampari.ttf',(44, 53, 57), poem)
     return render_template('index.html')
 
 if __name__ == "__main__":
