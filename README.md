bd7542930a521b83bb1ac5eb42df5a0e89eca3d7
nava <navaneethanit@gmail.com>
2014-08-06 13:22:16 +0530

    redesign

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
+<<<<<<< HEAD
+poembackground
+==============
+
+Tamil Poem background designer
+=======
 nava-app
 ========
+>>>>>>> 87715e80f1066e6abc4332cf64ec039098092cc9
--- app.py
+++ app.py
@@ -1,50 +1,12 @@
-from flask import Flask, request, render_template, send_from_directory, flash, redirect, url_for
-from redis import Redis
-
-from models import fetch_by_name
-from main import crawler_machine
-from config import REDIS_CRAWLER_KEY
-
 import os
+from flask import Flask
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY') or 't0p s3cr3t'
-redis = Redis()
 
-if app.debug:
-    import logging
-    from logging.handlers import RotatingFileHandler
-    file_handler = RotatingFileHandler('app.log', 'a', 1 * 1024 * 1024, 10)
-    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    app.logger.setLevel(logging.DEBUG)
-    file_handler.setLevel(logging.DEBUG)
-    app.logger.addHandler(file_handler)
-    #app.logger.info('microblog startup')
+@app.route("/")
+def hello():
+    return "Hello world!"
 
-@app.route('/', methods=('GET', 'POST'))
-def index():
-    return redirect(url_for('.view_search'))
-    
-@app.route('/search', methods=('GET', 'POST'))
-def view_search():
-    keyword = request.form.get('Search',None)
-    search_results = ''
-    print "Input:",keyword
-    if keyword:
-        search_results = fetch_by_name(keyword)
-    return render_template('index.html', search_results=search_results, keyword=keyword)
-
-@app.route('/recrawl', methods=('GET', 'POST'))
-def recrawl():
-    kw = request.args.get('search',None)
-    if kw is not None:
-        redis.sadd(REDIS_CRAWLER_KEY,kw)
-        #crawler_machine(kw)
-        flash("you have rescheduled it for '%s'.It will happen sooner or later" %kw)
-    return redirect(url_for('.view_search'))
-
-
-
-if __name__ == '__main__':
-    
-    app.run(port=8802, debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
